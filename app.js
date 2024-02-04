a8b77ca96938c09891cefaa2fc909291fea5c0d9
Rua <rua.elkasheef@gmail.com>
2024-02-03 22:01:16 -0500

    learning promises

---
+++ app.js
@@ -0,0 +1,71 @@
+// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+
+// const emailRef = document.querySelector(".email");
+// console.log(emailRef);
+
+// 1) THEN Method
+
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+//   });
+
+// 2) Async/Await Method
+
+// async function main (){
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+//     const data = await response.json()
+//     console.log (data)
+//     emailRef.innerHTML = data.email
+// }
+
+// main ();
+
+// const statusRef = document.querySelector(".status")
+
+// THEN MOTTHOD
+
+// getSubscriptionStatus().then (response => console.log (response))
+
+// ASYNC/AWAIT METHOD
+
+// async function main() {
+//     const status = ( await getSubscriptionStatus())
+//     statusRef.innerHTML = status
+// }
+
+// main ();
+
+// CREAT A FUNCTION
+
+const statusRef = document.querySelector(".status");
+
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    resolve("VIP");
+  });
+}
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  console.log(getVideo(status));
+}
+
+main();
